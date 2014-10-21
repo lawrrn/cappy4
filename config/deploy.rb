f20251a96adc0d167235456aed4a3e3c7558264c
rain <rain@rainbin.com>
2014-10-20 23:17:20 -0700

    fixed

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :repo_url, 'ssh://git@github.com:lawrrn/cappy4.git'
 # Default deploy_to directory is /var/www/my_app
 # set :deploy_to, '/var/www/my_app'
 
-set :user, "deployer"
+set :user, "asyn"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 
 # Default value for :scm is :git
