7e00787365d0c8aa66418c6199e0d7723e8e7654
rain <rain@rainbin.com>
2014-10-20 23:32:32 -0700

    fix9

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'cappy4'
-set :repo_url, 'git@github.com:lawrrn/cappy4.git'
+set :repo_url, 'ssh://git@bitbucket.org/lawrrn/cappy.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
